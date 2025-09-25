f6620432cb7b66518cf17ba6ace10c262d0d6640
Sina Mahmudmoradi <sina.mahmudmoradi@gmail.com>
2025-09-25 19:32:44 +0330

    add config

--- cmd/blog/main.go
+++ cmd/blog/main.go
@@ -1,9 +1,26 @@
 package main
 
-import "github.com/Sina-Mahmoodmoradi/blog/internal/delivery/http"
+import (
+	"log"
+
+	"github.com/Sina-Mahmoodmoradi/blog/internal/delivery/http"
+	"github.com/Sina-Mahmoodmoradi/blog/pkg/config"
+	"github.com/joho/godotenv"
+)
 
 
 func main(){
+
+	if err:=godotenv.Load();err!=nil{
+		log.Println("no .env file found (continuing with system env)")
+	}
+
+	cfg,err:= config.LoadFromEnv()
+	if err!=nil{
+		log.Fatalf("failed to load config: %v",err)
+	}
+	_ = cfg
+
 	r := http.SetupRouter()
 
 	r.Run(":8080")
