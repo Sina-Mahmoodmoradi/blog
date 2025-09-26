40c0ca735c342ffde64513d0ccf4e1d2b497cafe
Sina Mahmudmoradi <sina.mahmudmoradi@gmail.com>
2025-09-26 17:16:48 +0330

    add Connect()

---
+++ pkg/database/database.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Sina-Mahmoodmoradi/blog/pkg/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+
+
+
+
+
+func Connect(cfg *config.Config)(*gorm.DB, error){
+
+	dns := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=UTC",
+		cfg.DBHost,
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBName,
+		cfg.DBPort,
+	)
+
+	db,err := gorm.Open(postgres.Open(dns),&gorm.Config)
+
+	if err!=nil{
+		return nil,fmt.Errorf("db connection failed: %w",err)
+	}
+
+	log.Println("Connected to DB!")
+	
+	return db,nil
+
+}
