dac64b61e99243ff5547929ce47a53213f748952
Sina Mahmudmoradi <sina.mahmudmoradi@gmail.com>
2025-10-01 15:57:36 +0330

    add: add user

--- internal/delivery/http/router.go
+++ internal/delivery/http/router.go
@@ -1,7 +1,10 @@
 package http
 
 import (
+	"github.com/Sina-Mahmoodmoradi/blog/infrastructure/passwordhasher"
+	"github.com/Sina-Mahmoodmoradi/blog/infrastructure/repository"
 	"github.com/Sina-Mahmoodmoradi/blog/internal/delivery/http/handler"
+	"github.com/Sina-Mahmoodmoradi/blog/internal/usecase"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
@@ -11,9 +14,22 @@ import (
 func SetupRouter(db *gorm.DB)*gin.Engine{
 	r := gin.Default()
 
+	//health check
 	healthHandler := handler.NewHealthHandler(db)
 	healthHandler.RegisterRoutes(r)
 
+	//passwordHasher
+	hasher := passwordhasher.NewBcryptHasher()
 
+	//Repos
+	userRepo := repository.NewUserRepository(db)
+
+	//UseCase
+	userUseCase := usecase.NewUserUseCase(userRepo,hasher)
+
+	//handler
+	handler := handler.NewUserHandler(userUseCase)
+	handler.RegisterRoutes(r)
+	
 	return r
 }
