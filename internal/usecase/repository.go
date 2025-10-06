8e7bf6cf3b5ceab8d55bb4c36d5194fedb3f97b7
Sina Mahmudmoradi <sina.mahmudmoradi@gmail.com>
2025-10-06 12:27:43 +0330

    add findbyid

--- internal/usecase/repository.go
+++ internal/usecase/repository.go
@@ -9,6 +9,7 @@ import (
 
 type UserRepository interface{
 	FindByEmail(ctx context.Context,email string) (*entity.User, error)
+	FindByID(ctx context.Context,id uint) (*entity.User, error)
 	FindByUsername(ctx context.Context,username string) (*entity.User, error)
 	Save(ctx context.Context,user *entity.User) error
 }
