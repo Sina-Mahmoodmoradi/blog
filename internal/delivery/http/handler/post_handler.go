9a03ded2eccced58d96696344c8e6c89e33ec83f
Sina Mahmudmoradi <sina.mahmudmoradi@gmail.com>
2025-10-13 18:54:53 +0330

    add getposts method

--- internal/delivery/http/handler/post_handler.go
+++ internal/delivery/http/handler/post_handler.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"net/http"
 
-
 	"github.com/Sina-Mahmoodmoradi/blog/internal/delivery/http/middleware"
 	"github.com/Sina-Mahmoodmoradi/blog/internal/usecase"
 	"github.com/gin-gonic/gin"
@@ -28,6 +27,21 @@ type CreatePostResponse struct{
 	Content string `json:"content"`
 }
 
+type PaginationQuery struct{
+	Page int `form:"page" binding:"min=1"`
+	Limit int `form:"limit" binding:"min=1,max=100"`
+}
+
+type PostResponse = CreatePostResponse
+
+type GetPostsResponse struct{
+	Posts []PostResponse `json:"posts"`
+	Total int 			 `json:"total"`
+	Page int 			 `json:"Page"`
+	Limit int 			 `json:"limit"`
+
+}
+
 func NewPostHandler(useCase *usecase.PostUseCase,tokenManager usecase.TokenManager) *postHandler{
 	return &postHandler{
 		useCase: useCase,
@@ -40,6 +54,7 @@ func (h *postHandler)RegisterRoutes(r *gin.Engine){
 	auth.Use(middleware.AuthMiddleware(h.tokenManager))
 	{
 		auth.POST("/",h.Create)
+		auth.GET("/",h.GetPosts)
 	}
 }
 
@@ -53,6 +68,7 @@ func (h *postHandler)Create(c *gin.Context){
 	userID,ok := c.Get("userID")
 	if !ok{
 		c.JSON(http.StatusUnauthorized,gin.H{"error":"unauthorized"})
+		return
 	}
 
 
@@ -64,12 +80,60 @@ func (h *postHandler)Create(c *gin.Context){
 	})
 	if err!=nil{
 		c.JSON(http.StatusInternalServerError,gin.H{"error":err.Error()})
+		return
 	}
 
 	c.JSON(http.StatusCreated,&CreatePostResponse{
 		ID: ucResponse.ID,
 		Title: ucResponse.Title,
 		Content: ucResponse.Content,
+	})
+
+
+}
+
+
+func (h *postHandler)GetPosts(c *gin.Context){
+	q := PaginationQuery{
+		Page: 1,
+		Limit: 10,
+	}
+
+	if err:=c.ShouldBindQuery(&q);err!=nil{
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+		return
+	}
+
+	userID ,ok:=c.Get("userID") 
+	if !ok{
+		c.JSON(http.StatusUnauthorized,gin.H{"error":"unauthorized"})
+		return
+	}
+	ctx := c.Request.Context()
+	paginatedPosts,err :=h.useCase.GetAllPosts(ctx,&usecase.GetPostsRequest{
+		AuthorID: userID.(uint),
+		Page: q.Page,
+		Limit: q.Limit,
+	})
+	if err!=nil{
+		c.JSON(http.StatusInternalServerError,gin.H{"error":err.Error()})
+		return
+	}
+
+	posts := make([]PostResponse,0,len(paginatedPosts.Posts))
+	for _,post:= range paginatedPosts.Posts{
+		posts = append(posts, PostResponse{
+			ID: post.ID,
+			Title: post.Title,
+			Content: post.Content,
+		})
+	}
+
+	c.JSON(http.StatusOK,&GetPostsResponse{
+		Posts: posts,
+		Total: paginatedPosts.Total,
+		Page:paginatedPosts.Page,
+		Limit: paginatedPosts.Limit,
 	})
 
 
