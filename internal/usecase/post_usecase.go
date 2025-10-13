a2f8c9d2e879e19c413338f8cfc185a65eab2ade
Sina Mahmudmoradi <sina.mahmudmoradi@gmail.com>
2025-10-13 18:54:24 +0330

    add getallposts method

--- internal/usecase/post_usecase.go
+++ internal/usecase/post_usecase.go
@@ -20,8 +20,17 @@ type CreatePostRequest struct{
 	AuthorID uint
 }
 
-type CreatePostResponse struct{
+type GetPostsRequest struct{
+	AuthorID uint
+	Page int
+	Limit int
+}
 
+type PaginatedPosts struct{
+	Posts []entity.Post
+	Total int
+	Page int
+	Limit int 
 }
 
 
@@ -44,4 +53,24 @@ func (u *PostUseCase)CreatePost(ctx context.Context,req *CreatePostRequest)(*ent
 	}
 
 	return post,nil
+}
+
+
+func (u *PostUseCase)GetAllPosts(ctx context.Context,req *GetPostsRequest)(*PaginatedPosts,error){
+	offset:= (req.Page-1)*req.Limit
+	posts, err:= u.repo.GetList(ctx,req.AuthorID,offset,req.Limit)
+	if err!=nil{
+		return nil,fmt.Errorf("failed to get posts:%w",err)
+	}
+	count,err := u.repo.Count(ctx,req.AuthorID)
+	if err!=nil{
+		return nil,fmt.Errorf("failed to get count of posts:%w",err)
+	}
+
+	return &PaginatedPosts{
+		Posts: posts,
+		Total: count,
+		Page: req.Page,
+		Limit: req.Limit,
+	},nil
 }
