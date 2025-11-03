39b08aa6d454aa3bc7a9ba1c7ebd6d942d4a7097
Sina Mahmudmoradi <sina.mahmudmoradi@gmail.com>
2025-11-03 17:58:36 +0330

    add tag

--- internal/delivery/http/router.go
+++ internal/delivery/http/router.go
@@ -30,21 +30,26 @@ func SetupRouter(db *gorm.DB,cfg *config.Config)*gin.Engine{
 	userRepo := repository.NewUserRepository(db)
 	postRepo := repository.NewPostRepository(db)
 	commentRepo := repository.NewCommentRepository(db)
+	tagRepo := repository.NewTagRepository(db)
 
 	//UseCase
 	userUseCase := usecase.NewUserUseCase(userRepo,hasher,tokenManager)
 	postUseCase := usecase.NewPostUseCase(postRepo,commentRepo)
 	commentUseCase := usecase.NewCommentUseCase(commentRepo,postRepo)
+	tagUseCase := usecase.NewTagUseCase(tagRepo,postRepo)
+
 
 	//handler
 	userHandler := handler.NewUserHandler(userUseCase,tokenManager)
 	postHandler := handler.NewPostHandler(postUseCase,tokenManager)
 	commentHandler := handler.NewCommentHandler(commentUseCase,tokenManager)
-	
+	tagHandler := handler.NewTagHandler(tagUseCase,tokenManager)
+
 	//registering routes
 	userHandler.RegisterRoutes(r)
 	postHandler.RegisterRoutes(r)
 	commentHandler.RegisterRoutes(r)
+	tagHandler.RegisterRoutes(r)
 
 	return r
 }
